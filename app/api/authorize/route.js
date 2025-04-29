b31be5a9ac3a7161894468bdc82fd3cab3932bce
Damien Tanner <damien@tanner.me>
2025-04-29 14:55:10 +0100

    authroization api route

---
+++ app/api/authorize/route.js
@@ -0,0 +1,31 @@
+export const dynamic = "force-dynamic";
+import { NextResponse } from "next/server";
+
+// Endpoint called from client to get a client session key which lets the browser connect to the Layercode pipeline
+export const POST = async (request) => {
+  // Here you could do any user authorization checks you need for your app
+  const endpoint = "https://api.layercode.com/v1/pipelines/authorize_session";
+  const apiKey = process.env.LAYERCODE_API_KEY;
+  const requestBody = await request.json();
+  if (!requestBody || !requestBody.pipeline_id) {
+    throw new Error("Missing pipeline_id in request body.");
+  }
+  try {
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(requestBody),
+    });
+    if (!response.ok) {
+      const text = await response.text();
+      throw new Error(text || response.statusText);
+    }
+    return NextResponse.json(await response.json());
+  } catch (error) {
+    console.log("Layercode authorize session response error:", error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
